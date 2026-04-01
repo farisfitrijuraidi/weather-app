d1d914d0fa923d0e8ae77a893c80a9323426eaa8
Faris Fitri <169568145+farisfitrijuraidi@users.noreply.github.com>
2026-04-01 23:14:54 +0800

    implement dynamic weather search with DOM integration and data filtering

--- src/dom.js
+++ src/dom.js
@@ -1,17 +1,27 @@
 import { fetchWeatherData } from './weather.js';
 
 const form = document.querySelector('form');
-const submit = document.querySelector('#submit');
 const userInput = document.querySelector('#text');
 const locationDisplay = document.querySelector('.user_location');
 const weatherData = document.querySelector('.weather-data');
+const descriptionDisplay = document.querySelector('.description');
+const feelsLikeDisplay = document.querySelector('.feelsLike');
+const humidityDisplay = document.querySelector('.humidity');
+const uvindexDisplay = document.querySelector('.uvindex');
+const precipitationDisplay = document.querySelector('.precipitation');
+const temperatureDisplay = document.querySelector('.temperature');
 
 form.addEventListener('submit', (e) => {
 	e.preventDefault();
-	locationDisplay.textContent = `${userInput.value}`;
 	fetchWeatherData(userInput.value)
 		.then((data) => {
-			weatherData.textContent = `Temperature: ${data.currentConditions.temp}°C, Conditions: ${data.currentConditions.conditions}`;
+			locationDisplay.textContent = `Location: ${data.address}`;
+			descriptionDisplay.textContent = `Description: ${data.description}`;
+			feelsLikeDisplay.textContent = `Feels Like: ${data.feelsLike}°C`;
+			humidityDisplay.textContent = `Humidity: ${data.humidity}%`;
+			uvindexDisplay.textContent = `UV Index: ${data.uvindex}`;
+			precipitationDisplay.textContent = `Precipitation: ${data.precipitation}%`;
+			temperatureDisplay.textContent = `Temperature: ${data.temperature}°C`;
 		})
 		.catch((error) => {
 			console.error('Error fetching weather data:', error);
--- src/weather.js
+++ src/weather.js
@@ -1,17 +1,28 @@
 const fetchWeatherData = async (cityName) => {
 	const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${cityName}?unitGroup=metric&key=VJQ66CNKJ3UTF8C8FFFMLG787`;
-	try {
-		const response = await fetch(url);
-		if (!response.ok) {
-			throw new Error('The server rejected our request');
-		}
-		const data = await response.json();
-		console.log(data);
-		return data;
-	} catch (error) {
-		console.error('Error fetching weather data:', error);
-		throw error;
+
+	const response = await fetch(url);
+	if (!response.ok) {
+		throw new Error('The server rejected our request');
 	}
+	const data = await response.json();
+	const address = data.resolvedAddress;
+	const description = data.description;
+	const feelsLike = data.currentConditions.feelslike;
+	const humidity = data.currentConditions.humidity;
+	const uvindex = data.currentConditions.uvindex;
+	const precipitation = data.currentConditions.precipprob;
+	const temperature = data.currentConditions.temp;
+
+	return {
+		address,
+		description,
+		feelsLike,
+		humidity,
+		uvindex,
+		precipitation,
+		temperature,
+	};
 };
 
 export { fetchWeatherData };
